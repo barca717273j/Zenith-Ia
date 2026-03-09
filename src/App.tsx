d6753e4aef548999db3b6d95d27e020e64881d37
barca717273j <140835124+barca717273j@users.noreply.github.com>
2026-03-09 19:55:03 -0300

    Refactor App component formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-export default function App() {
-  return (
-    <div style={{padding:40}}>
-      <h1>Zenith funcionando</h1>
-      <p>Se você está vendo isso, o React está ok.</p>
-    </div>
-  )
+export default function App () {
+return (
+‹div style={{padding:40}}>
+<h1>Zenith funcionando</h1>
+‹p›Se você está vendo isso, o React está ok. </p> </div>
+)
 }
