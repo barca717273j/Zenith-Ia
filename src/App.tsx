f24e3ef116b436bcd6970c0f18256aeea46cb8c1
barca717273j <140835124+barca717273j@users.noreply.github.com>
2026-03-09 19:41:45 -0300

    Update App component with new content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import Dashboard from "./Dashboard"
-
 export default function App() {
   return (
-    <div style={{padding:40,color:"white"}}>
-      <h1>Zenith</h1>
-      <Dashboard />
+    <div style={{padding:40}}>
+      <h1>Zenith funcionando</h1>
+      <p>Se você está vendo isso, o React está ok.</p>
     </div>
   )
 }
