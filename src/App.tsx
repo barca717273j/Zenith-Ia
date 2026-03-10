e012344564faf6cf123aa081655fe0a973bda6b6
barca717273j <140835124+barca717273j@users.noreply.github.com>
2026-03-10 13:11:45 -0300

    Replace App component content with Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-export default function App () {
-return (
-‹div style={{padding:40}}>
-<h1>Zenith funcionando</h1>
-‹p›Se você está vendo isso, o React está ok. </p> </div>
-)
+import Dashboard from "./Dashboard"
+
+export default function App() {
+  return <Dashboard />
 }
